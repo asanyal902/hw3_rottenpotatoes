057397926ead8918402d2eb769edbcc4edb91e99
ayush <sanman@server.fake>
2013-03-17 01:50:27 +0530

    First Commit---hw3 works!

--- features/sort_movie_list.feature
+++ features/sort_movie_list.feature
@@ -23,9 +23,11 @@ Background: movies have been added to database
 
 Scenario: sort movies alphabetically
   When I follow "Movie Title"
+  Then I should see 'Aladdin' before 'Amelie'
+  And I should see 'The Incredibles' before 'The Terminator'
   # your steps here
 
 Scenario: sort movies in increasing order of release date
   When I follow "Release Date"
-  # your steps here
-
+  Then I should see 'Aladdin' before 'Amelie'
+  And I should see 'The Terminator' before 'The Incredibles' 
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,19 +2,21 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+  	Movie.create!(movie)
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
-Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+Then /I should see '(.*)' before '(.*)'/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  page.body.should =~ /#{e1}+.*#{e2}+/m
+  #flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -22,7 +24,22 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  rating_list.split(%r{\s*,\s*}).each do |name|
+	step %Q{I #{uncheck}check "ratings_#{name}"}
+  end
+end   			
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+
+
+ # flunk "Unimplemented"
+ 
+Then /I should( not)? see all of the movies/ do |orNot|
+  Movie.all.each do |movie|
+    
+      name = movie[:title]
+step %Q{I should#{orNot} see "#{name}"}
+  end
 end
+
